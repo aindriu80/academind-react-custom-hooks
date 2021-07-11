fe7e82cf6ce890b590805b86319d8ee1ef84b2ae
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-07-11 11:27:41 +0100

    Using Custom Hooks

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react'
 
-import Card from './Card'
-
 // hooks-> need to start with 'use' in function name
 const useCounter = () => {
   const [counter, setCounter] = useState(0)
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter((prevCounter) => prevCounter + 1)
@@ -13,7 +12,7 @@ const useCounter = () => {
     return () => clearInterval(interval)
   }, [])
 
-  return <Card>{counter}</Card>
+  return counter
 }
 
 export default useCounter
